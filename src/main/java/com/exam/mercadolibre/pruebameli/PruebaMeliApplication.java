4cb6713c4e85753ecef1e9e5569b43c7fa16e016
Yonathan Gonzalez <y.j.gonz.n@gmail.com>
2021-06-14 23:07:20 -0500

    initial commit

---
+++ src/main/java/com/exam/mercadolibre/pruebameli/PruebaMeliApplication.java
@@ -0,0 +1,13 @@
+package com.exam.mercadolibre.pruebameli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMeliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaMeliApplication.class, args);
+    }
+
+}
